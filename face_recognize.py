15a9e20139dd18c04e73b01aae7858da81118421
CHENTINGXIAN195467 <chentingxian195467@163.com>
2023-01-09 00:13:14 +0800

    第一次人脸识别的代码

--- face_recognize.py
+++ face_recognize.py
@@ -2,14 +2,14 @@ import cv2 as cv
 def fac_detect_demo():
 	# 将图片转为灰度图片
 	gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
 	# 加载特征数据
-	cv.CascadeClassifier('D:\Program Files\OpenCV\opencv\sources\data\haarcascades\haarcascade_frontalface_default.xml')
-
-
+	face_detector = cv.CascadeClassifier('D:\Program Files\OpenCV\opencv\sources\data\haarcascades\haarcascade_frontalface_default.xml')
+	faces = face_detector.detectMultiScale(gray)
+	for x,y,w,h in faces:
+		cv.rectangle(img, (x, y), (x + w, y + h), color=(0, 255, 0), thickness=2)
+	cv.imshow('result', img)
 # 加载图片
-img = cv.imread("./image/face.jpg")
+img = cv.imread("./image/wulin.jpeg")
 fac_detect_demo()
-cv.imshow('img', img)
 cv.waitKey(0)
 cv.destroyAllWindows()
