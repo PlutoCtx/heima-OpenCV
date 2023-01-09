8be18004631c688cb2e126e456c5cfde8b739de6
CHENTINGXIAN195467 <chentingxian195467@163.com>
2023-01-09 11:45:33 +0800

    检测多张人脸

--- face_recognize.py
+++ face_recognize.py
@@ -1,15 +1,43 @@
+"""
+	检测人脸
+"""
+import cv2
+
+# import cv2 as cv
+# def face_detect_demo():
+# 	# 将图片转为灰度图片
+# 	gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# 	# 加载特征数据
+# 	face_detector = cv.CascadeClassifier('D:\Program Files\OpenCV\opencv\sources\data\haarcascades\haarcascade_frontalface_default.xml')
+# 	faces = face_detector.detectMultiScale(gray)
+# 	for x, y, w, h in faces:
+# 		cv.rectangle(img, (x, y), (x + w, y + h), color=(0, 255, 0), thickness=2)
+# 	cv.imshow('result', img)
+# # 加载图片
+# img = cv.imread("./image/wulin.jpeg")
+# face_detect_demo()
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+
+
+"""
+	检测多张人脸
+"""
 import cv2 as cv
-def fac_detect_demo():
+__all__ = [cv2]
+def face_detect_demo():
 	# 将图片转为灰度图片
 	gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 	# 加载特征数据
 	face_detector = cv.CascadeClassifier('D:\Program Files\OpenCV\opencv\sources\data\haarcascades\haarcascade_frontalface_default.xml')
 	faces = face_detector.detectMultiScale(gray)
-	for x,y,w,h in faces:
-		cv.rectangle(img, (x, y), (x + w, y + h), color=(0, 255, 0), thickness=2)
+	for x, y, w, h in faces:
+		cv.rectangle(img, (x, y), (x + w, y + h), color=(0, 0, 255), thickness=2)
+		cv.circle(img, center=(x+w//2, y+h//2), radius=w//2, color=(0, 255, 0), thickness=2)
+	# 显示图片
 	cv.imshow('result', img)
 # 加载图片
-img = cv.imread("./image/wulin.jpeg")
-fac_detect_demo()
+img = cv.imread("./image/faces.png")
+face_detect_demo()
 cv.waitKey(0)
 cv.destroyAllWindows()
