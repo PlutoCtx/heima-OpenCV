0ae2de225ed095ba515cf5f33c43bc3fd4166a8b
PlutoCtx <chentingxian195467@163.com>
2023-01-18 10:31:15 +0800

    小改进，但没太大软用

---
+++ OpenCV-project02/__init__.py
@@ -0,0 +1,11 @@
+# @Version: python3.10
+# @Time: 2023/1/18 9:35
+# @Author: MaxBorooks
+# @Email: chentingxian195467@163.com
+# @File: __init__.py.py
+# @Software: PyCharm
+# @User: chent
+
+"""
+	来自B站一天搞定人脸识别项目！学不会up主直接下跪！（python+opencv）
+"""
